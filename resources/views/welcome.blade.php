6cfcaa0771a8b847d350f4b139a258d5a949a497
mohammadreza <mohammadreza.developer78@gmail.com>
2020-01-07 11:43:06 +0330

    nemidonam

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
-                margin: 0;
+                margin: 1;
             }
 
             .full-height {
